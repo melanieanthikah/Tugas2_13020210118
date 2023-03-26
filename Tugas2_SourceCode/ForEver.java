170853f3cfadccaac15c24e74d9c4ec58960fa14
melanieanthikah <127604131+melanieanthikah@users.noreply.github.com>
2023-03-26 16:29:31 +0800

    Add files via upload

---
+++ Tugas2_SourceCode/ForEver.java
@@ -0,0 +1,19 @@
+/* NIM : 13020210147
+Nama : Melanie Anthikah Usrifan
+Waktu : 19/03/2023 23.00 PM */
+
+public class ForEver {
+
+	/**
+	 * @param args
+	*/ 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/* Program */
+		System.out.println("Program akan looping, akhiri dengan ^c");
+		while (true) 
+		{	System.out.print ("Print satu baris.....\n");
+
+		}
+	}
+}
